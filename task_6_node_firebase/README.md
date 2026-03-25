70224177b31b7aaa09e0c3a512f467dfe24efca9
Ahmed Hytham Ahmed Samir <ahmedhytham48@gmail.com>
2026-03-25 23:29:44 +0200

    firebase project

---
+++ task_6_node_firebase/README.md
@@ -0,0 +1,3 @@
+# task_6_node_firebase
+
+A new Flutter project.
---
+++ task_6_node_firebase/android/app/src/main/kotlin/com/example/task_6_node_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_6_node_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
